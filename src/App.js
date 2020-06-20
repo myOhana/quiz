b7bcb862b370abf0e58c763457de0ba65e276b4c
shahpreetk <preet25@somaiya.edu>
2020-06-20 13:03:55 +0530

    made categories page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import  Home from './components/Home';
-import ChooseCategory from './components/quiz/ChooseCategory';
+import ChooseCategories from './components/quiz/ChooseCategory';
 import Quiz from './pages/Quiz';
 import QuizSummary from './components/quiz/QuizSummary'
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
     <Route path='/' exact component={Home} />
-    <Route path='/categories' exact component={ChooseCategory} />
+    <Route path='/categories' exact component={ChooseCategories} />
     <Route path='/quiz' exact component={Quiz} />
     <Route  path='/summary' exact component={QuizSummary}/>
     </Router>
--- src/components/quiz/ChooseCategory.js
+++ src/components/quiz/ChooseCategory.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+// import Quiz from './Quiz';
 
-function ChooseCategory() {
+function ChooseCategories() {
     return (
-        <div>
-            <p>Choose your category here</p>
+      <div className='main'>
+      <Fragment>
+      <Helmet>
+      <title>Quiz App - Categories</title>
+    </Helmet>
+     <div className="container grid grid-cols-2 gap-6 mt-6">
+  
+        <Link category='General Knowledge' className="ml-auto bg-white text-purple-800 p-10 font-semibold rounded shadow mt-6" to='/quiz/gk'> 
+          General Knowledge
+        </Link>
+        <Link category='Science : Mathematics' className="ml-auto bg-white text-purple-800 p-10 font-semibold rounded shadow mt-6" to='/quiz/math'>
+          Mathematics
+        </Link>
+        </div>
+
+        </Fragment>
         </div>
     )
 }
 
-export default ChooseCategory
+export default ChooseCategories;
