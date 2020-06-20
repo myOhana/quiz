5829729fb516fc9fcd8e5f0280e7ad0b95ef3c6d
shahpreetk <preet25@somaiya.edu>
2020-06-20 11:44:16 +0530

    connected styles.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "../node_modules/@mdi/font/css/materialdesignicons.min.css";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+import "./styles/styles.scss";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
