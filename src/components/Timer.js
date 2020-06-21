797e938048582519b7887f3ae23c00b754002fca
shahpreetk <preet25@somaiya.edu>
2020-06-21 15:25:21 +0530

    time changed

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,8 +4,8 @@ import { useHistory } from "react-router-dom";
 
 const Timer = ({ score, currentIndex, wrongAnswers }) => {
   // const { initialMinute = 0, initialSeconds = 5 } = props;
-  const [minutes, setMinutes] = useState(2);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(59);
   const history = useHistory();
 
   useEffect(() => {
